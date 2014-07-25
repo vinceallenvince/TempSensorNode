a554a0a9e4755a3b8e3f6eb059f0b25f04fdea43
Vince Allen <vince@spotify.com>
2014-07-25 16:28:11 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,36 @@
+var Router = require("routes-router");
+var http = require("http");
+var st = require("st");
+var url = require('url');
+
+var Keen = require("keen.io");
+
+var app = Router();
+
+app.addRoute("/", function (req, res) {
+
+  var url_parts = url.parse(req.url, true);
+  var query = url_parts.query;
+  var projectId = query.projectId;
+  var writeKey = query.writeKey;
+  var temperature = query.temperature;
+
+  var client = Keen.configure({
+      projectId: projectId,
+      writeKey: writeKey
+  });
+
+  // send single event to Keen IO
+  client.addEvent("temperature", {"temperature": temperature}, function(err, res) {
+      if (err) {
+          console.log("Oh no, an error!");
+      } else {
+          console.log("Hooray, it worked!");
+      }
+  });
+
+
+});
+var server = http.createServer(app);
+server.listen(8000);
+
