a702186342703ca73c1f3d884018051b93f5503d
Vince Allen <vince@spotify.com>
2014-07-26 07:54:08 -0400

    Upddates response.

--- index.js
+++ index.js
@@ -29,18 +29,22 @@ app.addRoute("/", function (req, res) {
   if (projectId && writeKey && temperature) {
 
     // send single event to Keen IO
-    client.addEvent("temperature", {"temperature": temperature}, function(err, res) {
+    client.addEvent("temperature", {"temperature": temperature}, function(err, result) {
         if (err) {
             console.log("Oh no, an error!");
+            res.writeHead(200, { "Content-Type": "text/html" });
             res.end("Oh no, an error!");
         } else {
             console.log("Hooray, it worked! Saved temp: " + temperature);
+            res.writeHead(200, { "Content-Type": "text/html" });
             res.end("Hooray, it worked! Saved temp: " + temperature);
         }
     });
 
   } else {
     console.log("query parameter missing!");
+
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end("query parameter missing!");
   }
 
